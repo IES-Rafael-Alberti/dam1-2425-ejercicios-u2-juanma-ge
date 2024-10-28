d7a729574b51700e57c9d4e8de7f417c1feaf8c8
juanma-ge <gejuan@iesrafaelalberti.es>
2024-10-28 20:45:07 +0100

    ola

--- src/ej22_11.py
+++ src/ej22_11.py
@@ -1,5 +1,15 @@
 def pedir_palabra(): 
     return input("Introduce una palabra: ")
 
-def separar_letras():
+def separar_letras(palabra):
+    for i in reversed():
+
+
+
+def main():
+    palabra = pedir_palabra()
+
+
+if __name__ == "__main__":
+    main()
     
