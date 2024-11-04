45b0382a3d62b83e55d2f89bc852ace8595f8e1d
juanma-ge <gejuan@iesrafaelalberti.es>
2024-11-04 13:13:48 +0100

    nuevo push

--- tests/test_21_01.py
+++ tests/test_21_01.py
@@ -1,6 +1,6 @@
 
 from src.ej21_01 import comprobar_edad
 
-def tests_comprobar_edad():
+def test_comprobar_edad():
     assert comprobar_edad(10) == "Usted es menor de edad"
     assert comprobar_edad(20) == "Usted es mayor de edad"
--- tests/test_21_06.py
+++ tests/test_21_06.py
@@ -1,6 +1,7 @@
 from src.ej21_06 import comparar_nombre_sexo
 
-assert comparar_nombre_sexo("Ana", "femenino") == "A"
-assert comparar_nombre_sexo("Luis", "masculino") == "B"
-assert comparar_nombre_sexo("Maria", "femenino") == "B"
-assert comparar_nombre_sexo("Pedro", "masculino") == "A"
+def test_comparar_nombre_sexo():
+    assert comparar_nombre_sexo("Ana", "femenino") == "A"
+    assert comparar_nombre_sexo("Luis", "masculino") == "B"
+    assert comparar_nombre_sexo("Maria", "femenino") == "B"
+    assert comparar_nombre_sexo("Pedro", "masculino") == "A"
